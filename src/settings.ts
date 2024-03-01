96d6a0930fdb8eae90f49a596b0dc30beaf0416c
Mara-Li <lili.simonetti@outlook.fr>
2024-03-01 09:58:38 +0100

    fix: merge with date variable

--- src/settings.ts
+++ src/settings.ts
@@ -202,21 +202,73 @@ export class SettingTab extends PluginSettingTab {
                         this.display();
                     }
                     ));
-            if (this.plugin.settings.deleteOriginal) {
-                new Setting(containerEl)
-                    .setName("Move to System Trash")
-                    .setDesc("If set to true, the original file will be moved to the system trash. Otherwise, it will be moved to the vault trash.")
-                    .addToggle(toggle => toggle
-                        .setValue(this.plugin.settings.moveToSystemTrash)
-                        .onChange(async (value) => {
-                            this.plugin.settings.moveToSystemTrash = value;
-                            await this.plugin.saveSettings();
-                        }
-                        ));
-            }
+        } else {
+            new Setting(containerEl)
+                .setName('Create link')
+                .setDesc('Add a link to the new file in the new vault to the current note. If set to false, the file will be left unchanged.')
+                .addToggle(toggle => toggle
+                    .setValue(this.plugin.settings.createLink)
+                    .onChange(async (value) => {
+                        this.plugin.settings.createLink = value;
+                        await this.plugin.saveSettings();
+                        this.display();
+                    })
+                );
         }
 
-        containerEl.createEl("h3", { text: "Other Settings" });
+        new Setting(containerEl)
+        .setName('Date variable')
+        .setHeading()
+        .setDesc("The date variable to use for the output folder or in override path. For folder, will use the folder note (inside, outside, or named index) data.");
+
+    new Setting(containerEl)
+        .setName("Date Variable")
+        .setDesc("The date variable to use for the output folder. If set to frontmatter, you can specify the key. If set to creation or modification, the date will be used.")
+        .addDropdown(dropdown => dropdown
+            .addOptions({
+                frontmatter: "Frontmatter",
+                creation: "Creation Date",
+                modification: "Modification Date"
+            })
+            .setValue(this.plugin.settings.dateVariable.type)
+            .onChange(async (value: "frontmatter" | "creation" | "modification") => {
+                this.plugin.settings.dateVariable.type = value;
+                await this.plugin.saveSettings();
+                this.display();
+            }
+            ));
+
+    if (this.plugin.settings.dateVariable.type == "frontmatter") {
+        new Setting(containerEl)
+            .setName("Frontmatter Key")
+            .setDesc("The key to use for the frontmatter date.")
+            .addText(text => text
+                .setPlaceholder("date")
+                .setValue(this.plugin.settings.dateVariable.frontmatterKey || "date")
+                .onChange(async (value) => {
+                    this.plugin.settings.dateVariable.frontmatterKey = value;
+                    await this.plugin.saveSettings();
+                }));
+
+        new Setting(containerEl)
+            .setName("Fallback Date")
+            .setDesc("The date to use if the frontmatter date is not set. Can be the last modification time or the file creation file.")
+            .addDropdown(dropdown => dropdown
+                .addOptions({
+                    creation: "Creation Date",
+                    modification: "Modification Date"
+                })
+                .setValue(this.plugin.settings.dateVariable.fallback || "creation")
+                .onChange(async (value: "creation" | "modification") => {
+                    this.plugin.settings.dateVariable.fallback = value;
+                    await this.plugin.saveSettings();
+                }
+                ));
+    }
+
+
+        new Setting(containerEl).setName('Other').setHeading();
+        
         new Setting(containerEl)
             .setName("Overwrite")
             .setDesc("If set to false, the file will be skipped if it already exists in the other vault.")
@@ -227,53 +279,7 @@ export class SettingTab extends PluginSettingTab {
                     await this.plugin.saveSettings();
                 }
                 ));
-        this.containerEl.createEl("h3", { text: "Date Variable" });
-        this.containerEl.createEl("p", { text: "The date variable to use for the output folder or in override path. For folder, will use the folder note (inside, outside, or named index) data." });
 
-        new Setting(containerEl)
-            .setName("Date Variable")
-            .setDesc("The date variable to use for the output folder. If set to frontmatter, you can specify the key. If set to creation or modification, the date will be used.")
-            .addDropdown(dropdown => dropdown
-                .addOptions({
-                    frontmatter: "Frontmatter",
-                    creation: "Creation Date",
-                    modification: "Modification Date"
-                })
-                .setValue(this.plugin.settings.dateVariable.type)
-                .onChange(async (value: "frontmatter" | "creation" | "modification") => {
-                    this.plugin.settings.dateVariable.type = value;
-                    await this.plugin.saveSettings();
-                    this.display();
-                }
-                ));
-
-        if (this.plugin.settings.dateVariable.type == "frontmatter") {
-            new Setting(containerEl)
-                .setName("Frontmatter Key")
-                .setDesc("The key to use for the frontmatter date.")
-                .addText(text => text
-                    .setPlaceholder("date")
-                    .setValue(this.plugin.settings.dateVariable.frontmatterKey || "date")
-                    .onChange(async (value) => {
-                        this.plugin.settings.dateVariable.frontmatterKey = value;
-                        await this.plugin.saveSettings();
-                    }));
-
-            new Setting(containerEl)
-                .setName("Fallback Date")
-                .setDesc("The date to use if the frontmatter date is not set. Can be the last modification time or the file creation file.")
-                .addDropdown(dropdown => dropdown
-                    .addOptions({
-                        creation: "Creation Date",
-                        modification: "Modification Date"
-                    })
-                    .setValue(this.plugin.settings.dateVariable.fallback || "creation")
-                    .onChange(async (value: "creation" | "modification") => {
-                        this.plugin.settings.dateVariable.fallback = value;
-                        await this.plugin.saveSettings();
-                    }
-                    ));
-        }
 
     }
 }
